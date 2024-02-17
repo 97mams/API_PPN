4f988407e6dab70673ebb01c5986c07af633095a
97mams <97mams@gmail.com>
2024-02-17 10:03:25 +0300

    creat layouts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import { createServer } from "node:http"
 const port = 3000
 const host = 'localhost'
+
+const callBack = (request, response) => {
+    const url = request.url
+    const route = new URL(url, `http://${host}:${port}`)
+    const enPoint = request.method + route.pathname
+    switch (enPoint) {
+        case 'GET/mams':
+            response.end(JSON.stringify({ name: "mams" }))
+            break;
+
+        default:
+            break;
+    }
+    response.end()
+}
+
 createServer(callBack)
     .listen(port, host, () => {
         console.log(`serve running at http://${host}:${port}`);
