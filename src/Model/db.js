4cf165decc0c75ee4fe302108c3df2b18d1c33ff
97mams <97mams@gmail.com>
2024-03-01 12:29:08 +0300

    change database to mysql2

--- src/Model/db.js
+++ src/Model/db.js
@@ -1,19 +1,9 @@
-import sqlite3 from "sqlite3";
+import mysql from 'mysql2'
 
-const dbName = "apiPPN.db"
-
-let db = new sqlite3.Database(dbName, err => {
-    if (err) throw err
-
-    console.log('Database started on: ', dbName)
-
-    db.run('DROP TABLE products')
-
-    // db.run('INSERT INTO products(name, price) VALUES(?, ?)', ['vary gasy', 3000])
-    // db.get('SELECT * FROM products', (err, data) => {
-    //     if (err) throw err
-
-    //     console.log(data);
-    // })
-})
+export const connect = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'apiPPN'
+}).promise()
 
