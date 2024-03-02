191d5752a92c231f7a42e9962eb03f808604a90c
97mams <97mams@gmail.com>
2024-03-02 18:16:07 +0300

    add INSERTE

--- src/Model/productModel.js
+++ src/Model/productModel.js
@@ -1,7 +1,20 @@
 import { connect } from "./db.js"
 
+/**
+ * @return {Promise<Products>}
+ */
 export const getProduct = async () => {
     const sql = "SELECT * FROM products"
     const [row] = await connect.query(sql)
     return row
+}
+
+/**
+ * @param {{name: string, type: string, price:Number}} params
+ * @return {Promise<Products>}
+ */
+export const addProduct = async ({ name, type, price }) => {
+    const sql = "INSERTE INTO products(name, type, price) VALUES(?,?,?)"
+    const [row] = connect.query(sql, [name, type, parseFloat(price)])
+    console.log(row);
 }
